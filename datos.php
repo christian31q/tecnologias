4d04722710adb2067952024e4f4b85a241040fd8
Christian Q <christian.quintero@unimilitar.edu.co>
2024-05-03 11:19:02 -0500

    ejercicio

--- datos.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$title = $_POST["title"]
-echo $title;
-
- 
-?>
--- js/app.js
+++ js/app.js
@@ -1,10 +1,35 @@
-window.onload = function () {
-    document.querySelector('form').addEventListener('input', (e) => {
-        const data = new FormData(e.currentTarget);
-        console.log(data.get('title'));
+window.onload = function (){
+  
+  document.addEventListener('submit', function (event) {
 
-        document.getElementById("texto").innerHTML = data.get('title');
+    // Prevent form from submitting to the server
+    event.preventDefault();
+    var formData = new URLSearchParams(new FormData(event.target));
 
-        
-      });
+
+
+    ajax("registro.php",formData);
+  
+  });
+}
+
+function serializeForm(form) {
+	var obj = {};
+	var formData = new FormData(form);
+	for (var key of formData.keys()) {
+		obj[key] = formData.get(key);
+	}
+	return obj;
+}
+
+function ajax(url,obj) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if(xhttp.readyState === 4 && xhttp.status === 200){
+          console.log(xhttp.response);
+        }           
+    }
+  xhttp.open("post", url, true);
+  xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhttp.send(obj.toString());
 }
