d1de6462abf301ec67108c59e55ed5901b3a57aa
Christian Q <christian.quintero@unimilitar.edu.co>
2024-05-17 10:12:25 -0500

    ejercicio cookies

--- js/data.js
+++ js/data.js
@@ -1,5 +1,8 @@
 window.onload = function(){
+    //document.cookie = "username=Christian";
+    //document.cookie = "correo=chri@umng.co";
+    //document.cookie = "animal=perro";
     
-      
+    console.log(document.cookie);
 
 }
---
+++ js/data1.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+
+    //document.cookie = "nombre=Christian";
+    //document.cookie = "correo=C@umng.co";
+    document.cookie = "animal=Perro";
+
+
+}
